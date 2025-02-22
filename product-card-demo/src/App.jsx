d16ca725b056f1ee5b67ed550e6e46a730ed4d01
Caio Freiats <caio-freitas@hotmail.com>
2025-02-22 17:37:47 -0500

    created folder/files for view product

--- product-card-demo/src/App.jsx
+++ product-card-demo/src/App.jsx
@@ -1,11 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import Header from "./header/Header";
+import ViewProduct from "./viewProduct/ViewProduct";
 
 function App() {
+  // create state for product data
+  const [product, setProduct] = useState(null);
+  //create state for loading
+  const [loading, setLoading] = useState(true);
   return (
     <>
       <Header />
+      <ViewProduct product={product} />
     </>
   );
 }
---
+++ product-card-demo/src/viewProduct/ViewProduct.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import "./viewProduct.css";
+function ViewProduct({ product }) {
+  return <div className="page-content">ViewProduct</div>;
+}
+
+export default ViewProduct;
